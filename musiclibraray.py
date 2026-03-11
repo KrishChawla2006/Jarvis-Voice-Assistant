da60954707a33b61f0a66a721192bec33a94cdcc
Krish Chawla <bhuminchawla@gmail.com>
2026-03-11 07:50:07 +0530

    Add music dictionary with song URLs

---
+++ musiclibraray.py
@@ -0,0 +1,6 @@
+music={
+    "stealth":"https://youtu.be/U47Tr9BB_wE",
+    "march":"https://youtu.be/TOsvDAw47ss?list=RDU47Tr9BB_wE",
+    "royal":"https://youtu.be/oOi3oJmfz4o?list=RDT7Oe1H87yi8",
+    "skyfall":"https://youtu.be/DeumyOzKqgI"
+}
